23def6f19ba1a0a61c3b93bf428d353b2b85c031
suja venkatesan <suja.venkatesan@Syncfusion.com>
2021-12-31 10:36:06 +0530

    Update the sample

---
+++ ChatXamarin/ChatXamarin/ChatXamarin/ViewModel/ViewModel.cs
@@ -0,0 +1,108 @@
+﻿using Syncfusion.XForms.Chat;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace ChatXamarin
+{
+   public class ViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<object> messages;
+
+        /// <summary>
+        /// current user of chat.
+        /// </summary>
+        private Author currentUser;
+
+        public ViewModel()
+        {
+            this.messages = new ObservableCollection<object>();
+            this.currentUser = new Author() { Name = "Nancy", Avatar = "People_Circle16.png" };
+            this.GenerateMessages();
+        }
+
+        /// <summary>
+        /// Gets or sets the group message conversation.
+        /// </summary>
+        public ObservableCollection<object> Messages
+        {
+            get
+            {
+                return this.messages;
+            }
+
+            set
+            {
+                this.messages = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current author.
+        /// </summary>
+        public Author CurrentUser
+        {
+            get
+            {
+                return this.currentUser;
+            }
+            set
+            {
+                this.currentUser = value;
+                RaisePropertyChanged("CurrentUser");
+            }
+        }
+
+        /// <summary>
+        /// Property changed handler.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Occurs when property is changed.
+        /// </summary>
+        /// <param name="propName">changed property name</param>
+        public void RaisePropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        private void GenerateMessages()
+        {
+            this.messages.Add(new TextMessage()
+            {
+                Author = currentUser,
+                Text = "Hi guys, good morning! I'm very delighted to share with you the news that our team is going to launch a new mobile application.",
+            });
+
+            this.messages.Add(new TextMessage()
+            {
+                Author = new Author() { Name = "Andrea", Avatar = "People_Circle2.png" },
+                Text = "Oh! That's great.",
+            });
+
+            this.messages.Add(new TextMessage()
+            {
+                Author = new Author() { Name = "Harrison", Avatar = "People_Circle14.png" },
+                Text = "That is good news.",
+            });
+
+            this.messages.Add(new TextMessage()
+            {
+                Author = new Author() { Name = "Margaret", Avatar = "People_Circle7.png" },
+                Text = "What kind of application is it and when are we going to launch?",
+            });
+
+            this.messages.Add(new TextMessage()
+            {
+                Author = currentUser,
+                Text = "A kind of Emergency Broadcast App.",
+            });
+        }
+    }
+}
